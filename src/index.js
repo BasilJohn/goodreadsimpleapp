30c1c5675875720fa011a238bcc22f33fb88458d
BasilJohn <ferdenend@gmail.com>
2018-09-12 03:38:21 +0530

    Commit Full
    Commit Full

--- src/index.js
+++ src/index.js
@@ -1,17 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import SearchBar from "../src/components/common/searchbar";
-import Styles from "./app.css";
-
-const Index = () => {
-  return (
-    <div className={Styles.App}>
-      <header className={Styles.ItemHeader}>
-        {"Find books by title, author, or ISBN"}
-      </header>
-      <SearchBar />
-    </div>
-  );
-};
-
-ReactDOM.render(<Index />, document.getElementById("index"));
+import FormContainer from "./js/components/container/FormContainer";
--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,54 @@
+require("babel-polyfill");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
-const htmlWebpackPlugin = new HtmlWebPackPlugin({
-    template: "./src/index.html",
-    filename: "./index.html"
-});
-let cookie;
-
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: true,
-                            importLoaders: 1,
-                            localIdentName: "[name]_[local]_[hash:base64]",
-                            sourceMap: true,
-                            minimize: true
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    
-    devServer: {
-        proxy: {
-            '/api/**':
-            {
-                target: 'https://www.goodreads.com',
-                pathRewrite:
-                    { '^/api': '' },
-                secure: true,
-                logLevel: 'debug'
-            }
-        },
-        headers:{
-
-            'Access-Control-Allow-Origin' : "*"
+  entry: ["babel-polyfill", "./src/index.js"],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    proxy: {
+      "/api/**": {
+        target: "http://localhost:3000",
+        pathRewrite: { "^/api": "" },
+        secure: true,
+        logLevel: "debug"
+      }
     },
-    plugins: [htmlWebpackPlugin]
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    }
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
 };
